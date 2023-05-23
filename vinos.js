d39ae91514d3d319bd8604ade930dbc56ac804b7
Leonel <leonelgiralde22@gmail.com>
2023-05-23 15:04:22 -0300

    first commit

---
+++ vinos.js
@@ -0,0 +1,236 @@
+var dataVinoteca = {
+    "fechaActual": "2022-01-01",
+    "Vinos": [
+      {
+        "id": 1,
+        "image": "images\Tinto-Fabre_Montmayour-2017.jpg",
+        "bodega": "Fabre_Montmayour",
+        "tipo": "Tinto",
+        "cepa" : "malbec",
+        "año": "2017",
+        "description": "Vino argentino Reserva, desde mendoza de uva 100% malbec.Color rojo intenso con tonos violetas. Aromas intensos de cerezas rojas y especias. Complejo y equilibrado con notas de vainilla y de chocolate proveniente de la crianza en barrica. Un vino moderno, con taninos suaves y sedosos. Perfecto con todo tipo de carnes a la parrilla, caza y quesos.Calidad argentina y experiencia francesa. Los invitamos a probar ese delicioso vino.",
+        "price": 5
+      },
+      {
+        "id": 2,
+        "image": "images\carmenere_viniterra_2022.webp",
+        "bodega": "Viniterra",
+        "tipo": "Tinto",
+        "cepa" : "Carmenere",
+        "año": "2022",
+        "description": "Carmenere es una uva oscura técnica originaria de la región francesa de Burdeos. Su segundo hogar fue la soleada república de Chile con su clima único y suelo rocoso. La cultura obtuvo su nombre de fr. carmín - uno de los tonos más brillantes de rojo que tiñe las hojas de la vid antes de la caída de las hojas.",
+        "price": 5
+      },
+      {
+        "id": 3,
+        "image": "images\Syrah-Trumpeter-2017.jpg",
+        "bodega": "Rutini",
+        "tipo": "Tinto",
+        "cepa" : "Syrah",
+        "año": "2017",
+        "description": "Muy aromático, en la nariz despliega aromas a grosella y frutillas maduras, bien combinados con acentos especiados (tabaco, vainilla, canela).Promete cierta calidez en boca y regala un posgusto amable y fragante.",
+        "price": 5
+      },
+      {
+        "id": 4,
+        "image": "images\Luigi_Bosca-Merlot-2004.jpg",
+        "bodega": "Luigi Bosca",
+        "tipo": "Tinto",
+        "cepa" : "Merlot",
+        "año": "2004",
+        "description": "Un vino tinto de Lujan de Cuyo, Mendoza. Este vino contiene notas de roble, vainilla y chocolate, el cual lo hace muy agradable en boca.",
+        "price": 5
+      },
+      {
+        "id": 5,
+        "image": "images\Pinot Noir-Bourgogne-2014.webp",
+        "bodega": "Bourgogne",
+        "tipo": "Tinto",
+        "cepa" : "Pinot Noir",
+        "año": "2014",
+        "description": "Vino Tinto Pinot Noir; Bourgogne 2014 el resultado de una larga historia, Borgoña y sus vinos son conocidos en todo el mundo. Pinot Noir es una variedad de uva autóctona de la zona. Bourgogne Pinot Noir es la denominación regional cuyos vinos pueden provenir de cualquier parte de Borgoña.",
+        "price": 5
+      },
+      {
+        "id": 6,
+        "image": "images\cabernet sauvignon- La linda-2018.jpg",
+        "bodega": "La linda",
+        "tipo": "Tinto",
+        "cepa" : "Cabernet Sauvignon",
+        "año": "2018",
+        "description": "Un tinto jugoso, con notas especiadas, gran cuerpo y estructura. Un Cabernet Sauvignon sobrio para acompañar incontables momentos",
+        "price": 5
+      },
+      {
+        "id": 7,
+        "image": "images\Chardonnay-Miolo-2014.jpeg",
+        "bodega": "Miolo",
+        "tipo": "Blanco",
+        "cepa" : "Chardonnay",
+        "año": "2014",
+        "description": "Color del vino Chardonnay Es un vino blanco, que en sus distintas presentaciones, puede tonarse amarillo pálido, con reflejos verdes o dorados",
+        "price": 5
+      },
+      {
+          "id": 8,
+          "image": "images\sauvignon blanc-blanco de blancos- 2014.webp",
+          "bodega": "blanco de blancos",
+          "tipo": "Blanco",
+          "cepa" : "Sauvignon blanc",
+          "año": "2014",
+          "description": "La uva sauvignon blanc da vinos muy nerviosos y aromáticos, de alcohol medio y acidez elevada. Consumidos jóvenes, los vinos son frescos, secos y muy aromáticos",
+          "price": 5
+        },
+        {
+          "id": 9,
+          "image": "images\torrontes- colome- 2007.jpg",
+          "bodega": "colome",
+          "tipo": "Blanco",
+          "cepa" : "Torrontes",
+          "año": "2007",
+          "description": "orrontés es un vino de color verdoso con leves tonalidades doradas. Es un blanco joven y por lo tanto expresa toda la frescura característica de este varietal",
+          "price": 5
+        },
+        {
+          "id": 10,
+          "image": "images\rosado seco- ramon bilbao- 2018.webp",
+          "bodega": " ramon bilbao",
+          "tipo": "Rosado",
+          "cepa" : "Seco",
+          "año": "2018",
+          "description": "El vino rosado es aquel que tiene algo del color típico del vino tinto, pero solo lo suficiente como para darle un color rosa, que puede ir del claro al fuerte casi violeta, según las uvas y las técnicas de producción usadas.",
+          "price": 5
+        },
+        {
+          "id": 11,
+          "image": "images\rosado dulce- finca las moras-2018.jpg",
+          "bodega": "finca las moras",
+          "tipo": "Rosado",
+          "cepa" : "dulce",
+          "año": "2018",
+          "description": "Un rosado es, básicamente, un vino tinto con poca maceración. Proviene de uvas negras (o mezcla de uvas negras y blancas) y la tonalidad del rosado dependerá del tiempo de maceración: a menor tiempo, color más tenue y  blanquecino; y a mayor tiempo de maceración, color más púrpura o violáceo. ",
+          "price": 5
+        },
+        {
+          "id": 12,
+          "image": "",
+          "bodega": "",
+          "tipo": "Tinto",
+          "cepa" : "malbec",
+          "año": "",
+          "description": "",
+          "price": 5
+        },
+        {
+          "id": 13,
+          "image": "",
+          "bodega": "",
+          "tipo": "Tinto",
+          "cepa" : "malbec",
+          "año": "",
+          "description": "",
+          "price": 5
+        },
+        {
+          "id": 14,
+          "image": "",
+          "bodega": "",
+          "tipo": "Tinto",
+          "cepa" : "malbec",
+          "año": "",
+          "description": "",
+          "price": 5
+        },
+        {
+          "id": 15,
+          "image": "",
+          "bodega": "",
+          "tipo": "Tinto",
+          "cepa" : "malbec",
+          "año": "",
+          "description": "",
+          "price": 5
+        },
+        {
+          "id": 16,
+          "image": "",
+          "bodega": "",
+          "tipo": "Tinto",
+          "cepa" : "malbec",
+          "año": "",
+          "description": "",
+          "price": 5
+        },
+        {
+          "id": 17,
+          "image": "",
+          "bodega": "",
+          "tipo": "Tinto",
+          "cepa" : "malbec",
+          "año": "",
+          "description": "",
+          "price": 5
+        },
+        {
+          "id": 18,
+          "image": "",
+          "bodega": "",
+          "tipo": "Tinto",
+          "cepa" : "malbec",
+          "año": "",
+          "description": "",
+          "price": 5
+        },
+        {
+          "id": 19,
+          "image": "",
+          "bodega": "",
+          "tipo": "Tinto",
+          "cepa" : "malbec",
+          "año": "",
+          "description": "",
+          "price": 5
+        },
+        {
+          "id": 20,
+          "image": "",
+          "bodega": "",
+          "tipo": "Tinto",
+          "cepa" : "malbec",
+          "año": "",
+          "description": "",
+          "price": 5
+        },
+        {
+          "id": 21,
+          "image": "",
+          "bodega": "",
+          "tipo": "Tinto",
+          "cepa" : "malbec",
+          "año": "",
+          "description": "",
+          "price": 5
+        },
+        {
+          "id": 22,
+          "image": "",
+          "bodega": "",
+          "tipo": "Tinto",
+          "cepa" : "malbec",
+          "año": "",
+          "description": "",
+          "price": 5
+        },
+        {
+          "id": 23,
+          "image": "",
+          "bodega": "",
+          "tipo": "Tinto",
+          "cepa" : "malbec",
+          "año": "",
+          "description": "",
+          "price": 5
+        },
+  
+    ]
+  }
